db3cff1f6626a292cb3985f0b1b91744dfd23fb8
undefined23333 <qq466311467@gmail.com>
2023-05-15 23:38:08 +0800

    update for IDEA

---
+++ src/ArrayTest.java
@@ -0,0 +1,23 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        MyTools mt = new MyTools();
+        int[] arr = {10, -1, 8, 0,34};
+        mt.bubble(arr);
+    }
+}
+
+
+class MyTools {
+    public void bubble(int[] arr){
+        int temp = 0;
+        for(int i=0; i<arr.length -1; i++){
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if(arr[j] > arr[j + 1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
